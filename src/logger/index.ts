b4d950278c6b3805b4ea4ee41fe2581c31b23fd1
Julian <juliandemourgues@gmail.com>
2022-07-09 00:54:36 +0200

    implement lazy loading

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -7,69 +7,76 @@ export class Logger {
     wLogger;
     withCw = false;
     
-    constructor() {
-        const EDU_LOG_GROUP = process.env.EDU_LOG_GROUP
-        const EDU_LOG_STREAM = process.env.EDU_LOG_STREAM
-        let transports: any = [
-            new winston.transports.Console({
-                format: winston.format.combine(
-                    winston.format.colorize(),
-                    winston.format.printf(info => `${new Date().toISOString()} ${info.level}: ${JSON.stringify(info.message)}`),
-                )
-            }),
-            new winston.transports.File({ filename: errorFilePath, level: 'error' }),
-            new winston.transports.File({ 
-                filename: logFilePath,
-                format: winston.format.combine(
-                    winston.format.timestamp(),
-                    winston.format.json()
-                )
-            })
-        ]
-        if (EDU_LOG_GROUP && EDU_LOG_STREAM) {
-            this.withCw = true
-            console.log(`Using remote log ${EDU_LOG_GROUP} and ${EDU_LOG_STREAM}`)
-            transports.push(new WinstonCloudWatch({
-                name: 'winston-cloudwatch',
-                logGroupName: EDU_LOG_GROUP,
-                logStreamName: EDU_LOG_STREAM,
-                awsRegion: 'eu-central-1',
-                jsonMessage: true
-            }))
+    constructor() {}
+
+    private _lazyLoadLogger() {
+        if (!this.wLogger) {
+            const EDU_LOG_GROUP = process.env.EDU_LOG_GROUP
+            const EDU_LOG_STREAM = process.env.EDU_LOG_STREAM
+            let transports: any = [
+                new winston.transports.Console({
+                    format: winston.format.combine(
+                        winston.format.colorize(),
+                        winston.format.printf(info => `${new Date().toISOString()} ${info.level}: ${JSON.stringify(info.message)}`),
+                    )
+                }),
+                new winston.transports.File({ filename: errorFilePath, level: 'error' }),
+                new winston.transports.File({ 
+                    filename: logFilePath,
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.json()
+                    )
+                })
+            ]
+            if (EDU_LOG_GROUP && EDU_LOG_STREAM) {
+                this.withCw = true
+                transports.push(new WinstonCloudWatch({
+                    name: 'winston-cloudwatch',
+                    logGroupName: EDU_LOG_GROUP,
+                    logStreamName: EDU_LOG_STREAM,
+                    awsRegion: 'eu-central-1',
+                    jsonMessage: true
+                }))
+            }
+            this.wLogger = winston.createLogger({
+                levels: winston.config.syslog.levels,
+                defaultMeta: {},
+                transports,
+            });
         }
-        this.wLogger = winston.createLogger({
-            levels: winston.config.syslog.levels,
-            defaultMeta: {},
-            transports,
-        });
-
-    }
-
-    debug = (message) => {
-        this.wLogger.debug(message)
-    }
-
-    info = (message) => {
-        this.wLogger.info(message)
-    }
-
-    warning = (message) => {
-        this.wLogger.warning(message)
-    }
-
-    error = (message) => {
-        this.wLogger.error(message)
-    }
-
-    alert = (message) => {
-        this.wLogger.alert(message)
     }
 
     flush = () => {
         if (this.withCw) {
             const cwTransport = this.wLogger.transports.find((t) => t.name === 'winston-cloudwatch')
-            cwTransport.kthxbye();
+            cwTransport.kthxbye(()=> {});
         }
+    }
+
+    debug = (message) => {
+        this._lazyLoadLogger()
+        this.wLogger.debug(message)
+    }
+
+    info = (message) => {
+        this._lazyLoadLogger()
+        this.wLogger.info(message)
+    }
+
+    warning = (message) => {
+        this._lazyLoadLogger()
+        this.wLogger.warning(message)
+    }
+
+    error = (message) => {
+        this._lazyLoadLogger()
+        this.wLogger.error(message)
+    }
+
+    alert = (message) => {
+        this._lazyLoadLogger()
+        this.wLogger.alert(message)
     }
 }
 
